4b4fd633770a682663639ba1b0791d15f72f88d0
The Kneb <sleepwell95@gmail.com>
2019-11-22 14:06:53 +0200

    Creating Basic NavMesh
    Creating basic NavNesh for later expansion on it.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //Geting AI library to use NavMesh
+
+public class Mover : MonoBehaviour {
+    [SerializeField] Transform target; //Assigning the target that we want to move to
+
+    // Update is called once per frame
+    void Update () {
+        GetComponent<NavMeshAgent> ().destination = target.position;
+        //Geting NavMesh on GameObject and assigning its destination position
+        //Then Player moves to target position.
+    }
+}
